34ab011000805911b70b1551ade2ff8169db6c87
Woo-JongHo <jonghowoo33@gmail.com>
2023-08-08 09:46:06 +0900

    [Wearth1.0] AdminDBManger까지

---
+++ src/main/java/com/example/demo/db/AdminDBManager.java
@@ -0,0 +1,33 @@
+package com.example.demo.db;
+
+import com.example.demo.vo.OrdersDetailGoodsVO;
+import com.example.demo.vo.OrdersVO;
+import com.example.demo.vo.UsersVO;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class AdminDBManager extends DBManager {
+    public static List<UsersVO> getTotalUserList() {
+
+        System.out.println("getTotalUserList의 AdminDBManager작동");
+
+        SqlSession session = sqlSessionFactory.openSession();
+        List<UsersVO> userList = session.selectList("admin.getTotalUserList");
+        System.out.println("AdminDBManger의 List확인" + userList);
+
+        session.close();
+        return userList;
+    }
+
+    public static int deleteUser(int userno){
+
+        int re = -1;
+        System.out.println();
+        SqlSession session =sqlSessionFactory.openSession();
+        re = session.delete("admin.deleteUser", userno);
+        session.close();
+        return re;
+    }
+}
